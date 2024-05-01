dd69c9cf84edade429e37d57afb0f6601703567d
Robson de Moraes Silva <rms2307@outlook.com>
2024-05-01 18:22:04 -0300

    Inicia implementação das primeiras telas

--- AppControleFinanceiro/App.xaml.cs
+++ AppControleFinanceiro/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace AppControleFinanceiro
+﻿using AppControleFinanceiro.Views;
+
+namespace AppControleFinanceiro
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new TransactionList();
         }
     }
 }
---
+++ AppControleFinanceiro/Views/TransactionList.xaml.cs
@@ -0,0 +1,15 @@
+namespace AppControleFinanceiro.Views;
+
+public partial class TransactionList : ContentPage
+{
+	public TransactionList()
+	{
+		InitializeComponent();
+	}
+
+	private void OnButtonClicked_To_TransactionAdd(object sender, EventArgs args)
+	{
+        App.Current.MainPage = new TransactionAdd();
+
+    }
+}
