502804197c579f882c89a722d4d94a839e47f803
Robson de Moraes Silva <rms2307@outlook.com>
2024-05-01 16:44:44 -0300

    Primeiro commit

---
+++ AppControleFinanceiro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppControleFinanceiro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
