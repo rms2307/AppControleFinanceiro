93af15083b3125b37e0344fc326308b3c4d8dfb5
Robson de Moraes Silva <rms2307@outlook.com>
2024-05-15 07:26:49 -0300

    Revert "Inicia implementação de tela de Add"
    This reverts commit ab2e1ef4a75b1f8b3aee93c0b576ab0c9a0043f3.

--- AppControleFinanceiro/Views/TransactionAdd.xaml.cs
+++ AppControleFinanceiro/Views/TransactionAdd.xaml.cs
@@ -2,41 +2,13 @@ namespace AppControleFinanceiro.Views;
 
 public partial class TransactionAdd : ContentPage
 {
-    public TransactionAdd()
-    {
-        InitializeComponent();
-    }
+	public TransactionAdd()
+	{
+		InitializeComponent();
+	}
 
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
-        Navigation.PopModalAsync();
-    }
-
-    public void OnButtonClicked_Save(object sencer, EventArgs e)
-    {
-
-    }
-
-    private bool IsValidData()
-    {
-        bool valid = true;
-
-        if (string.IsNullOrEmpty(EntryNome.Text) || string.IsNullOrWhiteSpace(EntryNome.Text))
-        {
-            valid = false;
-        }
-
-        if (string.IsNullOrEmpty(EntryValor.Text) || string.IsNullOrWhiteSpace(EntryValor.Text))
-        {
-            valid = false;
-        }
-
-        double result;
-        if (string.IsNullOrEmpty(EntryValor.Text) && !double.TryParse(EntryValor.Text, out result))
-        {
-            valid = false;
-        }
-
-        return valid;
+		Navigation.PopModalAsync();
     }
 }
