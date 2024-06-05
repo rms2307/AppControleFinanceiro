1304a5b0b12df985faf76be364fc00e9ee1268a2
Robson de Moraes Silva <rms2307@outlook.com>
2024-06-04 23:57:20 -0300

    Melhora layout das despesas com Converters

---
+++ AppControleFinanceiro/Libraries/Converters/TransactionValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using AppControleFinanceiro.Models;
+using System.Globalization;
+
+namespace AppControleFinanceiro.Libraries.Converters
+{
+    public class TransactionValueConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            Transaction? transaction = (Transaction)value;
+
+            if (transaction == null)
+            {
+                return string.Empty;
+            }
+
+            if (transaction.Type == TransactionType.Income)
+            {
+                return transaction.Value.ToString("C");
+            }
+            else
+            {
+                return $"- {transaction.Value:C}";
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
